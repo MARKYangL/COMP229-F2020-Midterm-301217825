aa4744f3533c979dfd4387027162480c3f38535d
Yang <90874019+MARKYangL@users.noreply.github.com>
2021-10-27 20:21:22 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -9,3 +9,8 @@ module.exports = {
   //local MongoDB deployment ->
   "URI": "mongodb://localhost:27017/comp229-f2020-midterm"
 };
+module.exports = {
+  //cloud MongoDB deployment ->
+  
+  "URI":"mongodb://midterm:DcJO86Vj5aa55bvWFkHqgujnPEWAjf9KiqCEMev4ns0u4S2MMGQZL1iPrZANnIAfJZSGozjpTK85AMKkHuj33A%3D%3D@midterm.mongo.cosmos.azure.com:10255/book?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@midterm@"
+};
